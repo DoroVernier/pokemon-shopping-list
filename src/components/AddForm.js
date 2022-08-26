5ac2450f3b4850f07a9f4fe22863d56f0b0e2ad6
AlexSna <asnatschko@gmail.com>
2022-08-26 15:51:58 +0200

    fixed prevetDDDDDDDefaultgit statusgit status

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,19 +1,22 @@
-export default function AddForm({newItem, onAddNewItem}){
+export default function AddForm({newShoppingList , onAddNewItem}){
   function handleSubmit(event){
-   event.preventdefault();
-   const inputValue = event.target.title.value
+   event.preventDefault();
+   
+   const form = event.target;
+   const inputValue = form.name.value;
+
    console.log(inputValue);
 
-   const addNewItem = {id:newItem + 1, name: inputValue};
+   const addNewItem = {id:newShoppingList + 1, name: inputValue};
    
-   onAddNewItem(newItem);
-  
+   onAddNewItem(addNewItem);
+  console.log(addNewItem);
   }
    
   return(
   <form onSubmit={handleSubmit}>
    <label htmlFor="add">
-    <input id="add" type="text" />
+    <input id="add" type="text" name="name"/>
       <button >
         Add Item</button>
     </label>
